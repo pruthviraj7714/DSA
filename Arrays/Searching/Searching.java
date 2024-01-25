abde59c301555da82125bcfe43b2c53e51152d76
pruthviraj7714 <marganepruthviraj@gmail.com>
2024-01-25 10:34:36 +0530

    searching & sorting is added

--- Arrays/Searching/Searching.java
+++ Arrays/Searching/Searching.java
@@ -2,10 +2,35 @@
 package Arrays.Searching;
 
 public class Searching {
+    public static int linearSearch(int arr[], int key) {//O(n)
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int arr[], int key) {//O(logn)
+        int start = 0, end = arr.length-1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == key) {
+                return mid;
+            }else if(arr[mid] > key) {
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        int key = 4;
+        System.out.println(linearSearch(arr, key));
+        System.out.println(binarySearch(arr, key));
         
-        System.out.println("hello wrold");
-
-
     }
 }
--- Arrays/Sorting/BasicSorting.java
+++ Arrays/Sorting/BasicSorting.java
@@ -1,3 +1,4 @@
+package Arrays.Sorting;
 
 
 public class BasicSorting {
@@ -18,9 +19,24 @@ public class BasicSorting {
             }
         }
     }
+    public static void selectionSort(int arr[]) {
+        for(int i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    
     public static void main(String[] args) {
         int arr[] = {6,2,1,5,3,4};
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        selectionSort(arr);
         printArray(arr);
 
 
